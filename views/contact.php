09682206fdb82fb969f8f7cd784646739d3e1e71
Brayden-I <bi1012440@otc.edu>
2025-11-12 14:17:49 -0600

    ts is finally working yay

---
+++ views/contact.php
@@ -0,0 +1,4 @@
+<h1>Contact Us</h1>
+<p>Email: info@coffeeshop.com</p>
+<p>Phone: (555) 123-4567</p>
+<p>Address: 123 Coffee Street, Brew City</p>
---
+++ views/home.php
@@ -0,0 +1,2 @@
+<h1>Welcome to Our Coffee Shop</h1>
+<p>The best coffee in town! Visit us for amazing beverages.</p>
